50580b1aa223345e903651c811f5719aa3eba60a
BryanACooke <bryancooke02@gmail.com>
2023-09-30 19:21:21 -0400

    Star System & Main Page

--- charity/src/App.js
+++ charity/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import landingBackground from './assets/landingBackgroundImage.png';
 import logo from './assets/hhLogo.png';
+import frontPageImage from './assets/imageFrontPage.png';
 
 function App() {
   return (
@@ -21,16 +22,19 @@ function App() {
 
     <div className="buttonContainer">
       <div className="getStartedButton">
-      <h1 className="innerButtonFont">Get started</h1>
+      <h1 className="innerButtonFont">Donate</h1>
       </div>
       
       <div className="getStartedButton">
-      <h1 className="innerButtonFont">Learn More</h1>
+      <h1 className="innerButtonFont">E-Volunteer</h1>
 
       </div>
+
       
     </div>
-
+    <div className="addImage">
+        <img className="frontPageImage" src={frontPageImage} alt="logo" />
+      </div>
 
 
     </div>
