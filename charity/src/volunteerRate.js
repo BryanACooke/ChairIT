715073f5b482968c4a63e9613c5cd33279434cd7
BryanACooke <bryancooke02@gmail.com>
2023-09-30 18:13:11 -0400

    Update

--- charity/src/volunteerRate.js
+++ charity/src/volunteerRate.js
@@ -1,29 +1,29 @@
 
 import './donateSubmitPage.css';
 import './VolunteerRate.css';
-import onehour from  './assets/onehour.png';
-import twohour from  './assets/twohours.png';
-import threehour from  './assets/threehours.png';
+import onehour from './assets/onehour.png';
+import twohour from './assets/twohours.png';
+import threehour from './assets/threehours.png';
 
 import starGrey from './assets/starGrey.png';
 import starYellow from './assets/starYellow.png';
 
-function VolunteerRate(){
-    return(
+function VolunteerRate() {
+    return (
         <div className="volunteerRateMainDiv backgroundColor">
             <div className="headerDiv">
-            <h1 className="whiteFont">Volunteering</h1>
-            <div className="rewardBadges">
-                <div className="rewardBadge">
-                    <img className="rewardBadgeImage" src={onehour} alt={`Preview`} />
+                <h1 className="whiteFont">Volunteering</h1>
+                <div className="rewardBadges">
+                    <div className="rewardBadge">
+                        <img className="rewardBadgeImage" src={onehour} alt={`Preview`} />
+                    </div>
+                    <div className="rewardBadge">
+                        <img className="rewardBadgeImage" src={twohour} alt={`Preview`} />
+                    </div>
+                    <div className="rewardBadge">
+                        <img className="rewardBadgeImage" src={threehour} alt={`Preview`} />
+                    </div>
                 </div>
-                <div className="rewardBadge">
-                <img className="rewardBadgeImage" src={twohour} alt={`Preview`} />
-                </div>
-                <div className="rewardBadge">
-                <img className="rewardBadgeImage" src={threehour} alt={`Preview`} />
-                </div>
-            </div>
             </div>
 
             <div className="objectHolder">
@@ -47,29 +47,36 @@ function VolunteerRate(){
                 </div>
                 <div className="parentRatingQualDiv">
 
-                <div className="rateQualityDiv">
-                    <h1> Please rate the quality of this item </h1>
+                    <div className="rateQualityDiv">
+                        <h1> Please rate the quality of this item </h1>
+                    </div>
+                    <div className="starRatingDiv">
+                        <input type="radio" id="star5" name="rating" value="5" />
+                        <label htmlFor="star5">
+                            <img className="greyStar" src={starGrey} alt="Star" />
+                        </label>
+
+                        <input type="radio" id="star4" name="rating" value="4" />
+                        <label htmlFor="star4">
+                            <img className="greyStar" src={starGrey} alt="Star" />
+                        </label>
+
+                        <input type="radio" id="star3" name="rating" value="3" />
+                        <label htmlFor="star3">
+                            <img className="greyStar" src={starGrey} alt="Star" />
+                        </label>
+
+                        <input type="radio" id="star2" name="rating" value="2" />
+                        <label htmlFor="star2">
+                            <img className="greyStar" src={starGrey} alt="Star" />
+                        </label>
+
+                        <input type="radio" id="star1" name="rating" value="1" />
+                        <label htmlFor="star1">
+                            <img className="greyStar" src={starGrey} alt="Star" />
+                        </label>
+                    </div>
                 </div>
-
-                <div className="starRatingDiv">
-                    <div className="hoverStarOne">
-                        <img className="greyStar" src={starGrey}></img>
-                    </div>
-                    <div className="hoverStarTwo">
-                        <img className="greyStar" src={starGrey}></img>
-                        </div>
-                    <div className="hoverStarThree">
-                        <img className="greyStar" src={starGrey}></img>
-                    </div>
-                    <div className="hoverStarFour">
-                        <img className="greyStar" src={starGrey}></img>
-                    </div>
-                    <div className="hoverStarFive">
-                        <img className="greyStar" src={starGrey}></img>
-                    </div>
-
-                    </div>
-            </div>
             </div>
 
 
