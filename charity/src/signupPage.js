7bd97fb5a298dfa92f07eb764d2ce3ecae534965
Aiyaan200 <125077174+Aiyaan200@users.noreply.github.com>
2023-09-30 18:47:25 -0400

    Update signupPage.js

--- charity/src/signupPage.js
+++ charity/src/signupPage.js
@@ -1,36 +1,43 @@
 import './signUpPage.css';
-import { Navigate } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import {DonationInformation} from './donationInfo.js';
+import React, { useState, useEffect } from "react";
 
 
 
 function SignInPage(){
-    let navigate = useNavigate();
+    const [Email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    function submitHandler(){
-        navigate('/DonationInformation')
+    const Login = () => {
+        const data = {Email,password}
+        console.log(data)
+        fetch("/login", {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((ress) => 
+        ress.json().then((returned) => {
+          console.log(returned)
+        })
+        );
     }
-
 
     return(
         <div className="backgroundColor signUpPageHome">
             <h1 className="personalInfoTitle">Personal Info</h1>
 
-<div className="formInputDiv">
-            <form className="formInput" onSubmit={submitHandler}>
-  <input className="inputFormName"  placeholder="Enter First Name" type="text" name="name" />
-  <input className="inputFormName"  placeholder="Enter Last Name" type="text" name="lastName" />
- <input className="inputFormName"  placeholder="Enter Email" type="text" name="enterEmail" />
-  <input className="inputFormName"  placeholder="Enter Phone Number" type="text" name="enterPhoneNumber" />
-  <input className="continueButton continueButtonMoreStyle" type="submit" value="Continue" />
+        <div className="formInputDiv">
+<form className="formInput" >
+
+    <input className="inputFormName" onChange={e => setEmail(e.target.value)} placeholder="Enter Email" type="text" name="enterEmail" />
+    <input className="inputFormName"  onChange={e => setPassword(e.target.value)} placeholder="Enter Password" type="password" name="enterPassword" />
+    <input className="continueButton continueButtonMoreStyle" type="button" value="Continue" onClick={Login}/>
 </form>
 
 </div>
         </div>
     );
-
-
 
 }
 
