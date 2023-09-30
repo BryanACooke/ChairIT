61d8a9d9a74e1c70045a181b3c17178d6a3c7eea
Aiyaan200 <125077174+Aiyaan200@users.noreply.github.com>
2023-09-30 18:45:08 -0400

    Update Flask_API.py

--- Flask_API.py
+++ Flask_API.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import session
 import mysql.connector
 
 endpoint = "localhost"
@@ -19,36 +20,80 @@ Cursor = Connection.cursor()
 app = Flask(__name__)
 app.secret_key= "iforgotthekey"
 
-@app.route("/")
+@app.route("/data")
 def start():
     return({"Status": "Online"})
 
 
-@app.route("/login", method=['GET'])
+@app.route("/login", methods=['POST'])
 def login():
-    log = request.json()
-
-    Login_Sql = "SELECT Password FROM user WHERE Email  = '%s' " %(log['Email'])
+    print("login")
+    log = request.get_json()
+    print(log['password'])
+    Login_Sql = "SELECT UserID, Password FROM user WHERE Email  = '%s' " %(log['Email'])
     Cursor.execute(Login_Sql)
     result = Cursor.fetchall()
-
-    if result == log['Password']:
-        return("Logged In")
-    elif result != log['Password']:
-        return("Login Failed")
+    for x in result:
+        print(x)
+    if result == []:
+        print("Invaild credentials")
+        return({"Status": False})
+    elif result[0][1] == log['password']:
+        session['UserID'] = result[0][0]
+        return({"Status": True})
+    elif result != log['password']:
+        return({"Status":False})
     else:
-        return("Invaild")
+        return({"Status":False})
 
-@app.route("/signup", method=['POST'])
+@app.route("/signup", methods=['POST'])
 def signup():
-    new = request.json()
-
+    new = request.get_json()
     New_sql = "INSERT INTO user (Name,Status,MarAcc,Password,ProPhotoPath,Personal,Email,PhoneNum) VALUES ()"
 
-@app.route('/Rerate', method=['PATCH'])
+
+@app.route("/addproduct", methods=["POST"])
+def addproduct():
+    add = request.get_json()
+    add_sql = "Insert INTO product (Name,Rating,UserID,ImgPath,Description,NumRatings,Prodtype) Values (%s,%s,%s,%s,%s,1,%s)"
+
+
+@app.route('/get_image/<int:Product_id>')
+def getImg(Product_id):
+    get_sql = "SELECT Rating, ImgPath FROM product WHERE ProductID = '%s'" %(Product_id)
+    Cursor.execute(get_sql)
+    result = Cursor.fetchall()
+    return({
+        "Rating" : result[0][0],
+        "ImgPath" : result[0][1],
+    })
+
+
+@app.route('/Rerate', methods=['PATCH'])
 def rerate():
-    rating = request.json()
-    
+    rating = request.get_json()
+    rating['ProductID']
+    rerate_sql = "SELECT Rating, NumRatings FROM product WHERE ProductID = '%s'" %(rating['ProductID'])
+    Cursor.execute(rerate_sql)
+
+    Rates = Cursor.fetchall()
+    Rating = int(Rates[0])
+    Num = int(Rates[1])
+    NewRating = int(rating['Rating'])
+    UpdateRating = (Rating * Num + NewRating) / Num + 1
+    Num += 1
+    Update_Sql = "UPDATE product SET Rating = %s, NumRatings = %s" % (UpdateRating, Num)
+    Cursor.execute(UpdateRating)
+    Connection.commit()
+    return({"Status" : "New Rating set"})
+
+
+@app.route('/user_prod/<int:User_id>', methods = ['GET'])
+def products(User_id):
+    prod_Sql = "SELECT Name FROM product WHERE UserID = %s" %User_id
+    Cursor.execute(prod_Sql)
+    products = Cursor.fetchall()
+    return(products)
 
 if __name__ == '__main__':
     app.run(debug=True)
